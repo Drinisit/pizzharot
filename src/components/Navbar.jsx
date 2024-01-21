5c200b0c569251dde5995af05b6a5cd1b5e541cd
Drin <aldrinisit3196@gmail.com>
2024-01-21 19:10:13 +0800

    Fixing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,24 +12,22 @@ const Navbar = () => {
     ]
   return (
     <div className='w-full top-0 left-0'>
-        <div className='items-center'>
-            <div className='flex items-center py-6 px-10 justify-between'>
+        <div className='md:flex items-center justify-between md:px-10 px-7 md:py-6 '>
+         
                 <div>
-                    <img className='h-6 items-center cursor-pointer' src={Logo} alt="" />
+                    <img className='h-6 flex items-center cursor-pointer' src={Logo} alt="" />
                 </div>
             
                 <ul className='md:flex md:items-center text-white gap-4'>
                     {
                         Links.map((link) => (
                             <li key={link.name}>
-                                <a href={link.link}>{link.name}</a>
+                                <a href={link.link} className='hover:text-amber-500 duration-500'>{link.name}</a>
                             </li>
                         ))
                     }
                     <Button>Online Order</Button>
                 </ul>
-            
-            </div>
             
         </div>
 
