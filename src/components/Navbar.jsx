24b5b8bf0e106b7135038c5ced0e4ddb321be877
Drin <aldrinisit3196@gmail.com>
2024-01-22 09:25:22 +0800

    Fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,20 +15,22 @@ const Navbar = () => {
     let [open,setOpen] = useState(false);
   return (
     <div className='w-full top-0 left-0'>
-        <div className='md:flex items-center justify-between md:px-10 px-7 md:py-6 '>
+        <div className='md:flex items-center justify-between md:px-10 px-7 md:py-4 py-3 '>
          
-                <div className='py-3'>
+                <div className=''>
                     <img className='h-6 flex items-center cursor-pointer' src={Logo} alt="" />
                 </div>
                 <div onClick={()=>setOpen(!open)} className='text-2xl text-white absolute top-3 right-8 cursor-pointer md:hidden'>
-                    {open? <IoIosClose/> : <IoMdMenu/>}
-               
+
+                    {open? <IoIosClose/> : <IoMdMenu/> }
+                    
+
                 </div>
             
-                <ul className={`md:flex md:items-center text-white gap-4 md:pb-0 pb-12 absolute md:static bg-amber-500 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20' : 'top-[-490px]'} md:opacity-100 opacity-0 `}>
+                <ul className={`md:flex md:items-center text-white md:pb-0 pb-12 absolute md:static bg-amber-500 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-14 opacity-100' :'top-[-490px]'} md:opacity-100 opacity-0`}>
                     {
                         Links.map((link) => (
-                            <li key={link.name} className='md:my-0 my-4'>
+                            <li key={link.name} className='md:ml-8 md:my-0 my-4'>
                                 <a href={link.link} className='hover:text-amber-500 duration-500'>{link.name}</a>
                             </li>
                         ))
