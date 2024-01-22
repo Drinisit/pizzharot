7fd8a6f24d431420e0a078d817bfdafedfb95040
Drin <aldrinisit3196@gmail.com>
2024-01-23 06:13:17 +0800

    Adding image on pizza menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeroImage from '../assets/pizzharap-hero1.jpg'
+import Pizza from '../assets/hero-pizza.png'
 
 
 
@@ -8,18 +9,20 @@ const Hero = () => {
     <div className='w-full h-screen'>
       <div className='absolute bg-gradient-to-t from-slate-900 w-full h-full'></div>
       <img className='top-0 left-0 w-full h-screen object-cover' src={HeroImage} alt="" />
-        <div className='absolute top-0 w-full h-screen flex flex-col justify-center text-gray-200'>
-            <div className='left-[5%] max-w-[1240px] m-auto absolute p-4'>
-                <h1 className='text-4xl font-bold uppercase md:text-6xl drop-shadow-xl'>Nilibre ng pizza pero chinarot</h1>
+      <img className='absolute top-1/4 right-1/4' src={Pizza} alt="" />  
+        <div className='max-w-[1240px] mx-auto items-center p-4 top-0 w-full h-screen absolute flex flex-col justify-center text-gray-200'>
+           <div className=''>
+               <h1 className='text-4xl font-bold uppercase md:text-6xl drop-shadow-xl'>Nilibre ng pizza pero chinarot</h1>
 
                 <p className='max-w-[600px] drop-shadow-xl py-2 text-xl'>The most delicious pizza on planet after you eat you will forget to pay Kapag kasama ang barkada ang pamilya lalong lalo na ang jowa.</p>
 
                 <button className='bg-white text-black text-xl rounded-md py-2 px-4'>How to order</button> 
                 <button className='bg-white text-black text-xl rounded-md py-2 px-4 ml-8'>See more</button>
-            </div>
-            <div>
-                    
-            </div>
+             </div>   
+
+    
+                 
+     
         </div>
     </div>
   )
--- src/components/Menus.jsx
+++ src/components/Menus.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Pizza1 from '../assets/pizza1.png'
+import Pizza2 from '../assets/pizza2.png'
+import Pizza3 from '../assets/pizza3.png'
 
 const Menus = () => {
   return (
     <div className='w-full h-screen'>
+      <div className='max-w-[968px] py-20 text-gray-200 mx-auto items-center justify-center grid grid-cols-3'>
+          <div>
+              <img src={Pizza1} alt="" />
+              <h1>Pepperoni Pizza</h1>
+              <p>250</p>
+          </div>
+          <div>
+              <img src={Pizza2} alt="" />
+              <h1>Hawaiian Pizza</h1>
+              <p>250</p>
+          </div>
+          <div>
+              <img src={Pizza3} alt="" />
+              <h1>Cheese Pizza</h1>
+              <p>250</p>
+          </div>
+      </div>
 
     </div>
   )
