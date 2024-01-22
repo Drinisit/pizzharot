6c5161da4c219792ea9f2a50f38e6a5a612db980
Drin <aldrinisit3196@gmail.com>
2024-01-22 20:43:37 +0800

    adding hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import HeroImage from '../assets/hero-pizza.png'
-import Background from '../assets/bgwood.jpg'
+import HeroImage from '../assets/pizzharap-hero1.jpg'
+
+
 
 const Hero = () => {
   return (
     <div className='w-full h-screen'>
-      <img className='top-0 left-0 w-full h-screen object-cover' src={Background} alt="" />
-        <div className='absolute top-0 w-full h-screen flex flex-col justify-center text-white'>
+      <img className='top-0 left-0 w-full h-screen object-cover' src={HeroImage} alt="" />
+      <div className='bg-gradient-to-b from-slate-950 to-slate-500'></div>
+        <div className='absolute top-0 w-full h-screen flex flex-col justify-center text-gray-200'>
             <div className='left-[5%] max-w-[1240px] m-auto absolute p-4'>
                 <h1 className='text-4xl font-bold uppercase md:text-6xl drop-shadow-xl'>Nilibre ng pizza pero chinarot</h1>
 
