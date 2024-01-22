002e47938a73bac85a6e619dd1c076fb97a0c5b6
Drin <aldrinisit3196@gmail.com>
2024-01-22 16:33:55 +0800

    Adding hero image and details currently working on it

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import HeroImage from '../assets/hero-pizza.png'
+import Background from '../assets/bgwood.jpg'
 
 const Hero = () => {
   return (
-    <div className='w-full'>
-        <div className='grid grid-cols-2 justify-between'>
-            <div className='text-white'>
-                <h1>NILIBRE NG PIZZA PERO CHINAROT</h1>
+    <div className='w-full h-screen'>
+      <img className='top-0 left-0 w-full h-screen object-cover' src={Background} alt="" />
+      <div className='bg-black/30 absolute top-0 left-0 w-full h-screen '/>
+        <div className='absolute top-0 w-full h-screen flex flex-col justify-center text-white'>
+            <div className='left-[10%] max-w-[1240px] m-auto absolute p-4'>
+                <h1 className='text-4xl font-bold uppercase md:text-6xl drop-shadow-xl'>Nilibre ng pizza pero chinarot</h1>
 
-                <p>The most delicious pizza on planet after you eat you will forget to pay Kapag kasama ang barkada ang pamilya lalong lalo na ang jowa.</p>
+                <p className='max-w-[600px] drop-shadow-xl py-2 text-xl'>The most delicious pizza on planet after you eat you will forget to pay Kapag kasama ang barkada ang pamilya lalong lalo na ang jowa.</p>
 
-                <button>How to order</button> <button>See more</button>
+                <button className='bg-white text-black text-xl rounded-md py-2 px-4'>How to order</button> 
+                <button className='bg-white text-black text-xl rounded-md py-2 px-4 ml-8'>See more</button>
             </div>
             <div>
-                    <img className='h-96' src={HeroImage} alt="" />
+                    
             </div>
         </div>
     </div>
