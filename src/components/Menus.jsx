250f411037aa55d941a7d552c148b8ed52083fb6
Drin <aldrinisit3196@gmail.com>
2024-01-25 21:40:46 +0800

    Revert design on the original pizza section

--- src/components/Menus.jsx
+++ src/components/Menus.jsx
@@ -9,43 +9,50 @@ const Menus = () => {
   return (
     <div className='w-full relative px-6 py-20 h-screen'>
       <div className='max-w-[1240px] text-gray-200 mx-auto'>
-        <div className='w-full mb-32'>
-            <h1 className='pizzaria text-amber-500 font-boldbold text-center md:text-[7rem] text-[4rem]'>Pizza Harot</h1>
-            <p className='pizzaria text-amber-500 text-center md:text-[3rem] text-3xl mt-[-2rem]'>The Best selling Pizza</p>
+        <div className='w-full'>
+            <h1 className='pizzaria text-amber-500 font-boldbold text-center text-[4rem]'>Pizza Harot</h1>
+            <p className='pizzaria text-amber-500 text-center text-3xl'>The Best selling Pizza</p>
         </div>
-        <div className='grid sm:grid-cols-2 md:grid-cols-4 grid-cols-1 gap-4 sm:px-16 md:px-10 px-12'>
-            <div className='border flex flex-col border-amber-500 rounded-md py-6 px-4 md:mt-0  mt-24 '>
-                  <img className='mx-auto mt-[-8rem]' src={Pizza1} alt="" />
-                  <h1 className='text-xl font-medium text-center mt-[-1rem]'>Pepperoni Pizza</h1>
-                  <p className='text-center pb-1 font-medium'>&#8369;350</p>
-                  <p>Reprehenderit, assumenda qui cum voluptas accusamus minus tenetur error? Quasi odio alias ratione repellat aspernatur amet quia doloribus incidunt autem aperiam!</p>
-                  <button className='py-1 px-3 mt-2 cursor-pointer hover:text-amber-500'>Now Order</button>
-              </div>
-              <div className='border flex flex-col border-amber-500 rounded-md py-6 px-4 md:mt-0 mt-24 '>
-                  <img className='mx-auto mt-[-8rem]' src={Pizza2} alt="" />
-                  <h1 className='text-xl font-medium text-center mt-[-1rem]'>Hawaiian Pizza</h1>
-                  <p className='text-center pb-1 font-medium'>&#8369;350</p>
-                  <p>Tempora doloremque quo, accusamus nesciunt reprehenderit iste porro fugit sint voluptatum autem delectus, itaque eveniet saepe voluptatibus, dignissimos.</p>
-                  <button className='py-1 px-3 mt-2 cursor-pointer hover:text-amber-500'>Now Order</button>
-              </div>
-              <div className='border flex flex-col border-amber-500 rounded-md py-6 px-4 md:mt-0  mt-24'>
-                  <img className='mx-auto mt-[-8rem]' src={Pizza3} alt="" />
-                  <h1 className='text-xl font-medium text-center mt-[-1rem]'>Cheese Pizza</h1>
-                  <p className='text-center pb-1 font-medium'>&#8369;350</p>
-                  <p>Lorem, ipsum dolor sit amet. Magni doloremque molestiae dolore odio praesentium vel est provident debitis, sequi enim eius ut aperiam quo placeat natus nesciunt autem.</p>
-                  <button className='py-1 px-3 mt-2 cursor-pointer hover:text-amber-500'>Now Order</button>
-              </div>
-              <div className='border flex flex-col border-amber-500 rounded-md py-6 px-4 md:mt-0 mt-24'>
-                  <img className='mx-auto mt-[-8rem]' src={Pizza4} alt="" />
-                  <h1 className='text-xl font-medium text-center mt-[-1rem]'>Classic Pizza</h1>
-                  <p className='text-center pb-1 font-medium'>&#8369;350</p>
-                  <p>Eveniet dolor porro delectus, molestias tempore eius dolorum, dolorem iure asperiores cumque, accusamus animi! Aliquid distinctio libero laudantium  beatae impedit.</p>
-                  <button className='py-1 px-3 mt-2 cursor-pointer hover:text-amber-500'>Now Order</button>
-              </div>
+
+        <div className='w-full'>
+            <p className=''>The Orginals</p>
         </div>
+        <div className='py-10'>
+            <div className='grid sm:grid-cols-2 md:grid-cols-4 grid-cols-1 gap-4'>
+                <div className='border flex flex-col border-amber-500 rounded-md py-6 px-4'>
+                    <img className='' src={Pizza1} alt="" />
+                    <h1 className=''>Pepperoni Pizza</h1>
+                    <p className=''>&#8369;350</p>
+                    <p>Reprehenderit, assumenda qui cum voluptas accusamus minus tenetur error? Quasi odio alias ratione repellat aspernatur amet quia doloribus incidunt autem aperiam!</p>
+                    <button className='py-1 px-3 cursor-pointer hover:text-amber-500'>Now Order</button>
+                </div>
+                <div className='border flex flex-col border-amber-500 rounded-md py-6 px-4'>
+                    <img className='' src={Pizza2} alt="" />
+                    <h1 className=''>Hawaiian Pizza</h1>
+                    <p className=''>&#8369;350</p>
+                    <p>Tempora doloremque quo, accusamus nesciunt reprehenderit iste porro fugit sint voluptatum autem delectus, itaque eveniet saepe voluptatibus, dignissimos.</p>
+                    <button className='py-1 px-3 cursor-pointer hover:text-amber-500'>Now Order</button>
+                </div>
+                <div className='border flex flex-col border-amber-500 rounded-md py-6 px-4'>
+                    <img className='' src={Pizza3} alt="" />
+                    <h1 className=''>Cheese Pizza</h1>
+                    <p className=''>&#8369;350</p>
+                    <p>Lorem, ipsum dolor sit amet. Magni doloremque molestiae dolore odio praesentium vel est provident debitis, sequi enim eius ut aperiam quo placeat natus nesciunt autem.</p>
+                    <button className='py-1 px-3 cursor-pointer hover:text-amber-500'>Now Order</button>
+                </div>
+                <div className='border flex flex-col border-amber-500 rounded-md py-6 px-4'>
+                    <img className='' src={Pizza4} alt="" />
+                    <h1 className=''>Classic Pizza</h1>
+                    <p className=''>&#8369;350</p>
+                    <p>Eveniet dolor porro delectus, molestias tempore eius dolorum, dolorem iure asperiores cumque, accusamus animi! Aliquid distinctio libero laudantium  beatae impedit.</p>
+                    <button className='py-1 px-3 cursor-pointer hover:text-amber-500'>Now Order</button>
+                </div>
+            </div>
+        </div>
+        
         <div className='mt-6 grid sm:grid-cols-3 md:grid-cols-6 gap-4'>
-          <div className='text-2xl font-bold mt-4 sm:col-span-3 md:col-span-6'>
-            <h1>Other Pizza Flavor</h1>
+          <div className='mt-4 sm:col-span-3 md:col-span-6'>
+            <h1 className='text-2xl font-bold'>Other Pizza Flavor</h1>
           </div>
               <div className='border flex flex-col border-amber-500 rounded-md px-1 py-4 overflow-hidden'>
                   <div className='mt-[-5rem]'>
