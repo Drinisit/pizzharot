9d38e9468aa03b6cdb4598a47d3cdf4336047f33
Drin <aldrinisit3196@gmail.com>
2024-01-28 17:26:46 +0800

    Changing Bundle image

--- src/components/Bundle.jsx
+++ src/components/Bundle.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Bundle1 from '../assets/bundleSolo.png'
 import Bundle2 from '../assets/bundleDuo.png'
-import Bundle3 from '../assets/bundleBox.png'
+import Bundle3 from '../assets/bundleHampasLupa.png'
 const Bundle = () => {
   return (
     <div className='w-full px-6 py-16'>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 grid-cols-1 gap-4'>
-            <div className='flex flex-row md:col-span-2 md:row-span-2 col-span-1 row-span-1 mx-auto'>
+            <div className='cursor-pointer lex flex-row md:col-span-2 md:row-span-2 col-span-1 row-span-1 mx-auto'>
                 <img src={Bundle3} alt="" />
             </div>
-            <div className='flex flex-row'>
+            <div className='cursor-pointer flex flex-row'>
                 <img src={Bundle1} alt="" />
             </div>
-            <div className='flex flex-row'>
+            <div className='cursor-pointer flex flex-row'>
                 <img src={Bundle2} alt="" />
             </div>
          
