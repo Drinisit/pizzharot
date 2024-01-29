9b1e24d9009b3f37c7f407e917e071ac9c964788
Drin <aldrinisit3196@gmail.com>
2024-01-30 06:54:06 +0800

    Add fix style on footer

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Image1 from '../assets/coachApple.png'
 import Image2 from '../assets/coachSara.png'
