861b04114eb376eae78d9636e37be0fef9eb317f
drin <132683845+Drinisit@users.noreply.github.com>
2024-01-30 15:50:24 +0800

    Fix : navbar dropdown wont appear on small screen device

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import HeroImage from '../assets/pizzaBghero.jpg'
+import HeroImage from '../assets/pizzharap-hero1.jpg'
+import Pizza from '../assets/hero-pizza.png'
 
 
 
 const Hero = () => {
   return (
-    <div className='w-full relative'>
-      <div className='absolute bg-gradient-to-t from-slate-900 w-full h-full'/>
-      
+    <div className='w-full'>
+      <div className='absolute bg-gradient-to-t from-slate-900 w-full h-full mt-2'></div>
       <img className='top-0 left-0 w-full h-screen object-cover' src={HeroImage} alt="" />
-      <div className='max-w-[1240px] mx-auto'>
+      <img className='absolute top-1/4 right-1/4' src={Pizza} alt="" />  
         <div className='max-w-[1240px] mx-auto items-center p-4 top-0 w-full h-screen absolute flex flex-col justify-center text-gray-200'>
            <div className=''>
                <h1 className='text-4xl font-bold uppercase md:text-6xl drop-shadow-xl'>Nilibre ng pizza pero chinarot</h1>
@@ -19,7 +19,9 @@ const Hero = () => {
                 <button className='bg-white text-black text-xl rounded-md py-2 px-4'>How to order</button> 
                 <button className='bg-white text-black text-xl rounded-md py-2 px-4 ml-8'>See more</button>
              </div>   
-      </div>    
+
+    
+                 
      
         </div>
     </div>
